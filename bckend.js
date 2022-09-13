2dbd712256e87fd20af5bfb18ee48513aafb8b07
Manel <104002687+Mainewl@users.noreply.github.com>
2022-09-13 10:19:21 -0300

    Add files via upload

---
+++ bckend.js
@@ -0,0 +1,31 @@
+const nodemailer = require('nodemailer');
+
+const STMP_CONFIG = require('./config/smtp') 
+
+
+
+const transporter = nodemailer.createTransport({
+  host: STMP_CONFIG.host, 
+  port: STMP_CONFIG.port,
+  secure: true,
+  auth: {
+    user: STMP_CONFIG.user,
+    pass: STMP_CONFIG.pass,
+  },
+//   ignoreTSL: false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+async function run(){
+  const mailSent = await transporter.sendMail({
+    text: "something",
+    subject: "TESTE N°1 COM O SITE FALSO",
+    from: "Lorem Ipsum <jsmailtst@gmail.com",
+    to: ["jsmailtst@gmail.com", "manonha.s2@hotmail.com", "manonhaas.s2@gmail.com"],
+  });
+  console.log(mailSent)
+}
+
+run();
